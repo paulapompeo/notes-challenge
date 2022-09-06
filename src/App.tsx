81519c1e81146f2193940c3c6c89f75c6e9c7b75
Paula Pompeo <paula.pompeo@macaiapp.com>
2022-09-06 09:49:30 +0200

    edits hash

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import Routes from "./routes";
 
 // To “reset” the server’s database by changing session, change this hash:
-export const hash = "419ddd5d26e49d9d69d7cf7699a8b3a7";
+export const hash = "419ddd5d26e49d9d69d7cf7699a8b348";
 
 function App() {
   return <Routes />;
